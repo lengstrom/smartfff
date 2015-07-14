2cb3d4a6fa253a20bdb9a61e9cba50f3d224b89f
lengstrom <lengstrom@users.noreply.github.com>
2015-07-13 23:12:40 -0400

    started doing multithreaded tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate ncurses;
+
+
+use ncurses::*;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // Setup ncurses
+    initscr();
+    raw();
+
+    // Allow for keyboard
+    keypad(stdscr, true);
+
+    let input_handle = thread::spawn( || {
+        while (true) {
+            let ch = getch();
+            
+        }
+    });
+
+    printw("Hello, world!");
+    
+    refresh();
+    getch();
+    endwin();
 }
+
